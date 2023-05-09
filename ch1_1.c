40f90c869ee16e2c2422b350cce340dbcd242ff2
sun060607 <minsung060607@gmail.com>
2023-05-09 14:37:06 +0900

    1st_commit

---
+++ ch1_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+extern int errno;
+
+int main(){
+    if(access("text.txt",F_OK)==-1){
+        printf("errno=%d",errno);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# System Programming
+
+여기에 정리할 예정..
